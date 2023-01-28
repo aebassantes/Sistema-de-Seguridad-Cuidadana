a0f1d1a97317a594fa17db23f00b740e30d7b793
IRam24 <120762846+IRam24@users.noreply.github.com>
2023-01-28 15:33:45 -0500

    Create procesar.php

---
+++ procesar.php
@@ -0,0 +1,12 @@
+<?php
+    $nombre = $_GET['nombre'];
+    $cedula = $_GET['cedula'];
+    $telefono = $_GET['telefono'];
+    $correo = $_GET['correo'];
+    $ubicacion = $_GET['ubicacion'];
+    $denuncia = $_GET['denuncia'];
+    $Tipo = $_GET['tipo'];
+
+echo "<p>Su denuncia a sido registrada coorectamente!</p>";
+echo "Registrada a nombre de " . $nombre . "<br/>";
+
